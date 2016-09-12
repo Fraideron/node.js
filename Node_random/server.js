370058d0ac1465229b2ee20bc4069e08b3bf4c60
Fraideron <jiafen@yandex.ua>
2016-09-12 18:17:34 +0300

    add  link's for name and some information about one

--- Node_random/server.js
+++ Node_random/server.js
@@ -7,39 +7,39 @@
 var express = require('express');
 var app = express();
 
+var Names = require('./storage/user')
+var name = new Names();
+var randomName = "";
+
 
 // This responds with "Hello World" on the homepage
 app.get('/random/names', function (req, res) {
-    var Names = require('./storage/user')
-    var name = new Names();
-    console.log("Got a GET request for the homepage");
-    res.send('Random name is: ' + name.showName());
+    console.log("Got a GET request for the random names");
+    randomName = name.showName();
+    res.send('<a href="./'+ randomName +'">' + randomName);
 })
 
 
 // This responds a POST request for the homepage
-app.post('/', function (req, res) {
+app.get('/', function (req, res) {
     console.log("Got a POST request for the homepage");
     res.send('Hello POST');
 })
 
-// This responds a DELETE request for the /del_user page.
-app.delete('/del_user', function (req, res) {
-    console.log("Got a DELETE request for /del_user");
-    res.send('Hello DELETE');
-})
+
 
 // This responds a GET request for the /list_user page.
-app.get('/list_user', function (req, res) {
-    console.log("Got a GET request for /list_user");
-    res.send('Page Listing');
+app.get('/storage/json', function (req, res) {
+    console.log("Got a GET request for /storage/json");
+    res.send(name.showJSON());
 })
 
 // This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', function(req, res) {
-    console.log("Got a GET request for /ab*cd");
-    res.send('Page Pattern Match');
+app.get('/random/names/*', function(req, res) {
+    console.log("Got a GET request random name");
+    res.send(name.showInformationAboutName(randomName));
 })
+
 
 
 var server = app.listen(8081, function () {
--- Node_random/storage/user.js
+++ Node_random/storage/user.js
@@ -8,14 +8,28 @@ class Names{
 
     constructor(){
         this.names = require('./foreign_names.json');
+
     }
 
 
     showName(){
-        var num = Math.floor(Math.random() * this.names.length);
-        return this.names[num].name;
+        this.num = Math.floor(Math.random() * this.names.length);
+        return this.names[this.num].name;
     }
 
+    showJSON(){
+        return JSON.stringify(this.names );
+    }
+
+    showInformationAboutName(){
+        return "name: " + this.names[this.num].name + ";<br>" +
+            "gender: " + this.names[this.num].gender + ";<br>" +
+            "origin: " + this.names[this.num].origin + ";<br>" +
+            "PeoplesCount: " + this.names[this.num].PeoplesCount + ";<br>" +
+            "WhenPeoplesCount: " + this.names[this.num].WhenPeoplesCount + ";";
+    }
 }
+
+
 
 module.exports = Names;
